28e2c7bfb88e43001d69d3828722639863c84e4f
andre <andrefer483@gmail.com>
2022-03-21 20:36:58 -0300

    baseURL atualizada

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,4 @@ import { create } from "apisauce";
 
 export const api = create({
   baseURL: process.env.REACT_APP_API_URL,
-  // baseURL: "https://back-end-mypharma.herokuapp.com",
-  // baseURL: "http://localhost:3333",
 });
